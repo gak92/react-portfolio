f998901983d96db59be657122a9badf2eb2e45a7
Ghazanfar Ali <gak_92@hotmail.com>
2023-01-12 21:06:52 +0500

    Complete footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import './footer.css';
+import {FaFacebookF} from 'react-icons/fa';
+import {FaLinkedinIn} from 'react-icons/fa';
+import {FaTwitter} from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className="footer__logo">Ghazanfar Ali</a>
+
+      <ul className="permalinks">
+      <li><a href="#">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#experience">Experience</a></li>
+      <li><a href="#services">Services</a></li>
+      <li><a href="#portfolio">Portfolio</a></li>
+      <li><a href="#testimonials">Testimonials</a></li>
+      <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.facebook.com/ghazanfar.ali.31105"><FaFacebookF /></a>
+        <a href="https://www.linkedin.com/in/ghazanfar-ali-kz/"><FaLinkedinIn /></a>
+        <a href="https://twitter.com/gak_92"><FaTwitter /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Ghazanfar Ali Portfolio. All rights reserved.</small>
+      </div>
+    </footer>
   )
 };
 
