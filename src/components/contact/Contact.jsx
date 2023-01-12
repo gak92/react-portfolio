c5da8c04f95a8580b9d4a4fc5789e0c05d816ec8
Ghazanfar Ali <gak_92@hotmail.com>
2023-01-12 20:31:32 +0500

    Complet contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
 import './contact.css';
+import {MdOutlineMail} from 'react-icons/md';
+import {RiMessengerLine} from 'react-icons/ri';
 
 const Contact = () => {
+
+  const form = useRef();
+  console.log(form);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_wb47sxq', 'template_0itttz7', form.current, 'P_53m0Y8QtaGYykOt')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    e.target.reset();
+  };
+
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMail className="contact__option-icon"/>
+            <h4>Email</h4>
+            <h5>gaks4s92@gmail.com</h5>
+            <a href="mailto:gaks4s92@gmail.com" target="_blank">Send a message</a>
+          </article>
+
+          <article className="contact__option">
+            <RiMessengerLine className="contact__option-icon"/>
+            <h4>Messenger</h4>
+            <h5>Ghazanfar Ali</h5>
+            <a href="https://m.me/ghazanfar.ali.31105" target="_blank">Send a message</a>
+          </article>
+        </div>
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your Full Name" required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
+          <button type="submit" className="btn btn-primary">Send Message</button>
+        </form>
+      </div>
+
+    </section>
   )
 };
 
