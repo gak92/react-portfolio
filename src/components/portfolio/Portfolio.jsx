addb02b6b51a5a2b7fbaa67cc5cdea11ea794bfd
Ghazanfar Ali <gak_92@hotmail.com>
2023-01-12 16:47:52 +0500

    Complete portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,80 @@
 import React from 'react';
 import './portfolio.css';
+import IMG1 from '../../assets/portfolio1.jpg';
 
 const Portfolio = () => {
+
+  const data = [
+    {
+      id: 1,
+      image: IMG1,
+      title: 'Title 01',
+      github: 'https://github.com',
+      demo: 'https://github.com'
+    },
+    {
+      id: 2,
+      image: IMG1,
+      title: 'Title 01',
+      github: 'https://github.com',
+      demo: 'https://github.com'
+    },
+    {
+      id: 3,
+      image: IMG1,
+      title: 'Title 01',
+      github: 'https://github.com',
+      demo: 'https://github.com'
+    },
+    {
+      id: 4,
+      image: IMG1,
+      title: 'Title 01',
+      github: 'https://github.com',
+      demo: 'https://github.com'
+    },
+    {
+      id: 5,
+      image: IMG1,
+      title: 'Title 01',
+      github: 'https://github.com',
+      demo: 'https://github.com'
+    },
+    {
+      id: 6,
+      image: IMG1,
+      title: 'Title 01',
+      github: 'https://github.com',
+      demo: 'https://github.com'
+    },
+  
+  ];
+  
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className="portfolio__item">
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className="btn" target="_blank">GitHub</a>
+                  <a href={demo} className="btn btn-primary" target="_blank">Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+        
+      </div>
+    </section>
   )
 };
 
