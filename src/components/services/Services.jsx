7ce19ca3d949300838a84e092776d85d111bae51
Ghazanfar Ali <gak_92@hotmail.com>
2023-01-13 21:56:21 +0500

    Add details in the service section

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -3,6 +3,37 @@ import './services.css';
 import {BiCheck} from 'react-icons/bi';
 
 const Services = () => {
+  const service1 = [
+    "Implementation using HTML, CSS, and JavaScript",
+    "Creating and implementing responsive designs",
+    "Development of user interface components",
+    "Creating and implementing interactive elements",
+    "Optimizing the website for speed and performance",
+    "Cross-browser and cross-platform compatibility",
+    "Integration with analytics tools",
+    "Creating documentation for the website or application"
+  ];
+
+  const service2 = [
+    "Server-side scripting and programming",
+    "Database design and management",
+    "Creating and implementing APIs for web and mobile applications",
+    "Server and hosting management, including deployment",
+    "Security and user authentication implementation",
+    "Performance optimization and troubleshooting",
+    "Integration with third-party services and APIs"
+  ];
+
+  const service3 = [
+    "Writing and editing articles, blog posts",
+    "Creating visual content such as images, videos, and infographics",
+    "Researching and identifying trending topics and keywords",
+    "Optimizing content for search engines (SEO)",
+    "Content marketing strategy development and execution",
+    "Social media management and community engagement",
+    "Measuring and analyzing the performance of content"
+  ];
+
   return (
     <section id="services">
       <h5>What I Offer</h5>
@@ -12,100 +43,58 @@ const Services = () => {
 
         <article className="service">
           <div className="service__head">
-            <h3>UI/UX Design</h3>
+            <h3>Frontend Development</h3>
           </div>
 
           <ul className="service__list">
-            <li>
-              <BiCheck className="service__list-icon"/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon"/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon"/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon"/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon"/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon"/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            </li>
+            {
+              service1.map((service, index) => {
+                return (
+                  <li key={index}>
+                    <BiCheck className="service__list-icon"/>
+                    <p>{service}</p>
+                  </li>
+                )
+              })            
+            }
           </ul>
         </article>
 
         <article className="service">
           <div className="service__head">
-            <h3>Web Development</h3>
+            <h3>Backend Development</h3>
           </div>
 
           <ul className="service__list">
-            <li>
-              <BiCheck className="service__list-icon"/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon"/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon"/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon"/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon"/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon"/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            </li>
+            {
+              service2.map((service, index) => {
+                return (
+                  <li key={index}>
+                    <BiCheck className="service__list-icon"/>
+                    <p>{service}</p>
+                  </li>
+                )
+              })            
+            }
           </ul>
         </article>
 
         <article className="service">
           <div className="service__head">
-            <h3>Content Creation</h3>
+            <h3>SEO & Content</h3>
           </div>
 
           <ul className="service__list">
-            <li>
-              <BiCheck className="service__list-icon"/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon"/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon"/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon"/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon"/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon"/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            </li>
+            {
+              service3.map((service, index) => {
+                return (
+                  <li key={index}>
+                    <BiCheck className="service__list-icon"/>
+                    <p>{service}</p>
+                  </li>
+                )
+              })            
+            }
           </ul>
         </article>
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './testimonials.css';
-import AVATAR1 from '../../assets/avatar1.jpg';
+// import AVATAR1 from '../../assets/avatar1.jpg';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
