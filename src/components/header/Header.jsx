0cf9536445e86c10cc97006170cb5946babdb71c
Ghazanfar Ali <gak_92@hotmail.com>
2023-01-13 20:59:09 +0500

    Update animation text

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import './header.css';
 import CTA from './CTA';
 import ME from '../../assets/my_pic.png';
 import HeaderSocials from './HeaderSocials';
+import { TypeAnimation } from 'react-type-animation';
 
 const Header = () => {
   return (
@@ -10,7 +11,20 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Ghazanfar Ali</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <TypeAnimation className="text-light"
+          sequence={[
+            'Fullstack Developer', // Types 'One'
+            1000, // Waits 1s
+            'Freelancer', // Deletes 'One' and types 'Two'
+            2000, // Waits 2s
+            'Reading Enthusiast', // Types 'Three' without deleting 'Two'
+            1000,
+          ]}
+          wrapper="div"
+          cursor={true}
+          repeat={Infinity}
+          style={{ fontSize: '2em' }}
+        />
         <CTA />
 
         <HeaderSocials />
